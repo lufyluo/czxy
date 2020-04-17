b98811db8329d0c0cac8789fbd88a89b135878e8
lion-kk <2466896229@qq.com>
2020-04-17 11:49:56 +0800

    1:修改token验证功能

--- src/main/java/com/czxy/manage/infrastructure/aop/AuthorizationFilter.java
+++ src/main/java/com/czxy/manage/infrastructure/aop/AuthorizationFilter.java
@@ -10,6 +10,7 @@ import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
