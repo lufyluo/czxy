fca67e4e6eb9ba79e8329441dc21a71c3483644e
lion-kk <2466896229@qq.com>
2020-04-24 18:56:56 +0800

    1:完成班级信息查看学员信息和删除接口

--- src/main/java/com/czxy/manage/controller/ClassController.java
+++ src/main/java/com/czxy/manage/controller/ClassController.java
@@ -1,16 +1,19 @@
 package com.czxy.manage.controller;
 
+import com.czxy.manage.infrastructure.response.BaseResponse;
+import com.czxy.manage.infrastructure.response.ResponseUtil;
 import com.czxy.manage.model.PageParam;
+import com.czxy.manage.model.vo.classes.ClassInfo;
 import com.czxy.manage.model.vo.classes.ClassOrgInfo;
-import com.czxy.manage.model.vo.user.UserAccountInfo;
 import com.czxy.manage.service.ClassService;
 import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @Api("班级管理")
@@ -18,10 +21,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClassController {
     @Autowired
     private ClassService classService;
+
     @GetMapping("/page")
     @ApiOperation(value = "分页获取班级", notes = "分页获取班级")
     public PageInfo<ClassOrgInfo> page(PageParam<String> pageParam) {
         return classService.page(pageParam);
+    }
+
+    @GetMapping("/query")
+    @ApiOperation(value = "查看班级学员信息", notes = "传入ID为class id")
+    public List<ClassInfo> query(Integer id) {
+        return classService.query(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "删除班级信息", notes = "传入id的为class表的id")
+    public Boolean delete(@PathVariable Integer id) {
+        return classService.delete(id);
     }
 
 
--- src/main/java/com/czxy/manage/dao/ClassMapper.java
+++ src/main/java/com/czxy/manage/dao/ClassMapper.java
@@ -1,7 +1,9 @@
 package com.czxy.manage.dao;
 
+import com.czxy.manage.infrastructure.response.BaseResponse;
 import com.czxy.manage.model.entity.ClassEntity;
 import com.czxy.manage.model.entity.ClassOrgEntity;
+import com.czxy.manage.model.vo.classes.ClassInfo;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,4 +11,8 @@ import java.util.List;
 @Mapper
 public interface ClassMapper {
     List<ClassOrgEntity> queryAll(String param);
+
+    Integer delete(Integer id);
+
+    List<ClassInfo> query(Integer id);
 }
