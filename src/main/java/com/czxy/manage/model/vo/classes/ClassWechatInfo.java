1502d79bfdebc2456d97e5ba997a1c31e3ccf78a
lion-kk <2466896229@qq.com>
2020-06-18 00:14:29 +0800

    1:修改根据userID查询班级信息接口编写2

--- src/main/java/com/czxy/manage/model/vo/classes/ClassWechatInfo.java
+++ src/main/java/com/czxy/manage/model/vo/classes/ClassWechatInfo.java
@@ -1,5 +1,6 @@
 package com.czxy.manage.model.vo.classes;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -11,13 +12,16 @@ public class ClassWechatInfo {
     @ApiModelProperty("班级ID")
     private Integer id;
     private String name;
+    @JsonFormat(pattern = "yyyy年MM月dd日 HH时")
     private Date beginTime;
+    @JsonFormat(pattern = "yyyy年MM月dd日 HH时")
     private Date endTime;
     @ApiModelProperty("课程数量")
     private int amount;
     @ApiModelProperty("教学对象")
     private String compositionName;
     @ApiModelProperty("创建时间")
+    @JsonFormat(pattern = "yyyy年MM月dd日 HH时")
     private Date createdTime;
     @ApiModelProperty("学生数量")
     private Integer number;
